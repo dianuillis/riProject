311b9036b4f29eb6646a4c9b12946aee8c9fcdb7
Diana Ramirez <diana.ramirez@etu.univ-st-etienne.fr>
2016-11-09 12:10:00 +0100

    Query indexing function added

--- src/main/java/com/ujm/ri/App.java
+++ src/main/java/com/ujm/ri/App.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,7 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
 
 	private static SortedMap<String, SortedMap<Integer, Integer>> invertedIndex = new TreeMap<>();
-
+	private static SortedMap<String, SortedMap<Integer, Integer>> queryIndex = new TreeMap<>();
 	/*
 	 * extracting the id of each document
 	 */
@@ -66,23 +67,23 @@ public class App {
 	/*
 	 * building the inverted index, line by line, TODO explain more
 	 */
-	public static void buildIndex(String[] words, String doc) {
+	public static void buildIndex(String[] words, String doc, SortedMap<String, SortedMap<Integer, Integer>> index) {
 		// get the docId
 		Integer docId = Integer.parseInt(doc);
 		for (String word : words) {
-			if (invertedIndex.containsKey(word)) {
-				SortedMap<Integer, Integer> tmpMap = invertedIndex.get(word);
+			if (index.containsKey(word)) {
+				SortedMap<Integer, Integer> tmpMap = index.get(word);
 				if (tmpMap.containsKey(docId)) {
 					tmpMap.put(docId, tmpMap.get(docId) + 1);
-					invertedIndex.put(word, tmpMap);
+					index.put(word, tmpMap);
 				} else {
 					tmpMap.put(docId, 1);
-					invertedIndex.put(word, tmpMap);
+					index.put(word, tmpMap);
 				}
 			} else {
 				SortedMap<Integer, Integer> auxMap = new TreeMap<>();
 				auxMap.put(docId, 1);
-				invertedIndex.put(word, auxMap);
+				index.put(word, auxMap);
 			}
 		}
 	}
@@ -122,6 +123,49 @@ public class App {
 		fis.close();
 		System.out.println("Deserialized HashMap..\n");
 	}
+	
+	// Read query
+	
+	public static void parseQuery () throws IOException
+	{
+		File file = new File("docs/topics_M2DSC7Q_2016_17.txt");
+
+		LineIterator it = FileUtils.lineIterator(file, "UTF-8");
+		try {
+			while (it.hasNext()) {
+				String line = it.nextLine();
+				// if we reach a new document, we change the id
+				String[] splits = line.split(" ");
+				String queryId=splits[0];
+				String[] terms= new String [splits.length-1];
+				
+				for (int i=1; i<splits.length; i++)
+					{
+					String token= splits[i];
+					terms[i-1]= token;
+					}
+				buildIndex(terms, queryId, queryIndex);
+				
+				for (Entry<String, SortedMap<Integer, Integer>> entry : queryIndex
+						.entrySet()) {
+					String term = entry.getKey();
+					SortedMap<Integer, Integer> postings = entry.getValue();
+					System.out.print(term + " [");
+					for (Entry<Integer, Integer> littleEntry : postings.entrySet()) {
+						System.out.print(littleEntry.getKey() + ":"
+								+ littleEntry.getValue() + ";");
+					}
+					System.out.println("]");
+				}
+				
+			}
+		}
+		finally {
+			LineIterator.closeQuietly(it);
+		}
+	}
+	
+	
 
 	public static void main(String[] args) throws IOException,
 			ClassNotFoundException {
@@ -129,7 +173,9 @@ public class App {
 		Long start = System.currentTimeMillis();
 
 		String docId = "";
-
+		
+		parseQuery();
+		/*
 		File file = new File("docs/Text_Only_Ascii_Coll_MWI_NoSem");
 
 		
@@ -146,7 +192,7 @@ public class App {
 				} else {
 					String[] words = StringUtils.split(line);
 					String[] cleaned = cleanLine(words);
-					buildIndex(cleaned, docId);
+					buildIndex(cleaned, docId, invertedIndex);
 				}
 			}
 		} finally {
@@ -161,5 +207,11 @@ public class App {
 
 		System.out.println("*** Completed in : " + (end - start) / 1000
 				+ " s ***");
+				*/
 	}
+	
+	
+	
+	
+	
 }
